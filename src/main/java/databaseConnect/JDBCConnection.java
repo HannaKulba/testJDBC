b1e9ef9d358f9ca2f439c1ae1fdb6df38296dc84
Hanna Kulba <anna.mirraza@gmail.com>
2021-08-11 17:18:37 +0300

    add changes: - migrate to localhost - add logging - rewrite tests

--- src/main/java/databaseConnect/JDBCConnection.java
+++ src/main/java/databaseConnect/JDBCConnection.java
@@ -1,33 +1,34 @@
 package databaseConnect;
 
+import org.jooq.impl.DSL;
 import utils.Log;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class JDBCConnection {
-    private static final String host = "192.168.100.185";
-    private static final String url = "jdbc:mysql://" + host + ":3306/students";
-    private static final String user = "senla";
-    private static final String password = "senla2020";
+    private static final String host = "localhost";
+    private static final String DBName = "sakila";
+    private static final String url = "jdbc:mysql://" + host + ":3306/" + DBName;
+    private static final String user = "root";
+    private static final String password = "root";
 
     private static Connection con = null;
     private static Statement stmt = null;
     private static ResultSet rs = null;
 
     public static Connection connectToDB() {
+        Log.info("Connect to DB " + url + " by user " + user);
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            Log.error(e.getMessage());
-        }
-        Log.info("Connect to DB " + url + " by user " + user);
-        try {
-            // opening database connection to MySQL server
+            // открытие коннекта к MySQL server
             con = DriverManager.getConnection(url, user, password);
             Log.info("Connection to DB successful!");
+        } catch (ClassNotFoundException e) {
+            Log.error(e.getMessage());
         } catch (SQLException sqlEx) {
-            Log.error("Connection to DB failed!");
-            Log.error(sqlEx.getMessage());
+            Log.error("Connection to DB failed!\n" + sqlEx.getMessage());
         }
 
         return con;
@@ -37,27 +38,27 @@ public class JDBCConnection {
         if (con != null) {
             try {
                 con.close();
-                Log.info("Connection to DB closed");
+                Log.info("Connection to DB closed successfully");
             } catch (SQLException se) {
-                Log.error(se.getMessage());
+                Log.error("Connection to DB was not closed. Reason:\n" + se.getMessage());
             }
         }
 
         if (stmt != null) {
             try {
                 stmt.close();
-                Log.info("Statement closed");
+                Log.info("Statement closed successfully");
             } catch (SQLException se) {
-                Log.error(se.getMessage());
+                Log.error("Statement was not closed. Reason:\n" + se.getMessage());
             }
         }
 
         if (rs != null) {
             try {
                 rs.close();
-                Log.info("ResultSet closed");
+                Log.info("ResultSet closed successfully");
             } catch (SQLException se) {
-                Log.error(se.getMessage());
+                Log.error("ResultSet was not closed. Reason:\n" + se.getMessage());
             }
         }
     }
@@ -67,54 +68,78 @@ public class JDBCConnection {
             stmt = connectToDB().prepareStatement(query);
             Log.info("Send request to DB: " + query);
             stmt.executeUpdate(query);
-            Log.info("Table was created");
+            Log.info("Table was created successfully");
         } catch (SQLException se) {
-            Log.error(se.getMessage());
+            Log.error("Table was not created. Reason:\n" + se.getMessage());
         }
     }
 
-    public static ResultSet selectFromDB(String query) {
+    public static void dropTable(String tableName) {
+        String query = "DROP TABLE " + tableName;
+        try {
+            stmt = connectToDB().prepareStatement(query);
+            Log.info("Send request to DB: " + query);
+            stmt.executeUpdate(query);
+            Log.info("Table was deleted successfully");
+        } catch (SQLException se) {
+            Log.error("Table was not deleted. Reason:\n" + se.getMessage());
+        }
+    }
+
+    public static ResultSet selectFromTable(String query) {
         try {
             stmt = connectToDB().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             Log.info("Send request to DB: " + query);
             rs = stmt.executeQuery(query);
-            Log.info("Data from table was got");
+            //перемещение курсора к следующемей строке значений, удовлетворяющих запросу
+            rs.next();
+            //логирование результатов запроса
+            addSQLRequestResultsToLog(query);
         } catch (SQLException se) {
             Log.error(se.getMessage());
         }
         return rs;
     }
 
-    public static void insertIntoDB(String query) {
+    //метод необязательный, приведен в качестве примера
+    //логирование результатов запроса
+    private static void addSQLRequestResultsToLog(String query) {
+        StringBuilder builder = new StringBuilder();
+        DSL.using(connectToDB()).fetchStream(query)
+                .forEach(r -> builder.append(r.format()));
+        Log.info("Request results:\n" + builder);
+    }
+
+    public static void insertIntoTable(String query) {
         try {
             stmt = connectToDB().createStatement();
             Log.info("Send request to DB: " + query);
             stmt.executeUpdate(query);
-            Log.info("New data inserted into table");
+            Log.info("New data was inserted into table successfully");
         } catch (SQLException se) {
-            Log.error(se.getMessage());
+            Log.error("New data was not inserted into table. Reason:\n" + se.getMessage());
         }
     }
 
-    public static void updateInDB(String query) {
+    public static void updateInTable(String query) {
         try {
             stmt = connectToDB().createStatement();
             Log.info("Send request to DB: " + query);
             stmt.executeUpdate(query);
-            Log.info("Data in table was updated");
+            Log.info("Data in table was updated successfully");
         } catch (SQLException se) {
-            Log.error(se.getMessage());
+            Log.error("Data in table was not updated. Reason:\n" + se.getMessage());
         }
     }
 
-    public static void deleteFromDB(String query) {
+    public static void deleteFromTable(String query) {
         try {
             Log.info("Send request to DB: " + query);
             stmt = connectToDB().createStatement();
             stmt.executeUpdate(query);
-            Log.info("Data from table was deleted");
+            Log.info("Data from table was deleted successfully");
         } catch (SQLException se) {
-            Log.error(se.getMessage());
+            Log.error("Data from table was not deleted. Reason:\n" + se.getMessage());
         }
     }
 
--- src/test/java/tests/TestDatabase.java
+++ src/test/java/tests/TestDatabase.java
@@ -6,110 +6,102 @@ import org.junit.jupiter.api.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@DisplayName("Проверка подключения к БД students и отправки запросов")
+@DisplayName("Проверка подключения к БД sakila и отправки запросов")
 public class TestDatabase extends TestsSetups {
 
     @Test
     @Order(1)
-    @DisplayName("Проверка подключения к БД")
-    public void testConnection() {
-        Assertions.assertNotNull(JDBCConnection.connectToDB());
+    @DisplayName("Проверка создания таблицы в БД")
+    public void testCreateTable() {
+        String query = "CREATE TABLE citizens ("
+                + "ID int(6) NOT NULL,"
+                + "FIRST_NAME VARCHAR(45) NOT NULL,"
+                + "LAST_NAME VARCHAR(45) NOT NULL,"
+                + "TOWN VARCHAR(45),"
+                + "PRIMARY KEY (id))";
+        JDBCConnection.createTable(query);
     }
-
-//    @Test
-//    @Order(2)
-//    @DisplayName("Проверка создания таблицы в БД")
-//    public void testCreateTable()  {
-//        String query = "CREATE TABLE professor ("
-//                + "id int(6) NOT NULL,"
-//                + "first_name VARCHAR(45) NOT NULL,"
-//                + "last_name VARCHAR(45) NOT NULL,"
-//                + "discipline VARCHAR(45),"
-//                + "PRIMARY KEY (id))";
-//        JDBCConnection.createTable(query);
-//    }
 
     @Test
     @Order(2)
-    @DisplayName("Отправка простого SELECT запроса. Проверка имени студента")
-    public void testSelectRequest_checkFirstName() throws SQLException {
-        String query = "SELECT first_name, last_name FROM students WHERE id=10";
-        ResultSet rs = JDBCConnection.selectFromDB(query);
-        rs.first();
-        Assertions.assertEquals(rs.getString("first_name"), "Tatsiana");
+    @DisplayName("Отправка INSERT запроса")
+    public void testInsertRequest() {
+        String query = "INSERT INTO citizens (ID, FIRST_NAME, LAST_NAME, TOWN) VALUES (412890, 'Leonardo', 'Di Caprio', 'Los Angeles')";
+        JDBCConnection.insertIntoTable(query);
+
+        String selectQuery = "SELECT * FROM citizens";
+        ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
+        assertAll("Should return inserted data",
+                () -> assertEquals("412890", rs.getString("ID")),
+                () -> assertEquals("Leonardo", rs.getString("FIRST_NAME")),
+                () -> assertEquals("Di Caprio", rs.getString("LAST_NAME")),
+                () -> assertEquals("Los Angeles", rs.getString("TOWN")));
     }
 
     @Test
     @Order(3)
-    @DisplayName("Отправка SELECT JOIN запроса. Проверка фамилии студента")
-    public void testSelectRequest_checkLastName() throws SQLException {
-        String query = "SELECT first_name, last_name FROM students WHERE id=10";
-        ResultSet rs = JDBCConnection.selectFromDB(query);
-        rs.first();
-        Assertions.assertEquals(rs.getString("last_name"), "Larionova");
+    public void testUpdateRequest() throws SQLException {
+        String query = "UPDATE citizens SET TOWN = 'LONDON' WHERE ID='412890'";
+        JDBCConnection.updateInTable(query);
+        String selectQuery = "SELECT TOWN FROM citizens WHERE ID=412890";
+        ResultSet rs = JDBCConnection.selectFromTable(selectQuery);
+        String expectedTown = "LONDON";
+        String actualTown = rs.getString("TOWN");
+        assertEquals(expectedTown, actualTown, "Actual town is '" + actualTown + "'. Expected - '" + expectedTown + "'.");
     }
 
     @Test
     @Order(4)
-    @DisplayName("Отправка SELECT JOIN запроса. Проверка имени последнего в списке студента")
-    public void testSelectWithJoinRequest_CheckFistName() throws SQLException {
-        String query = "SELECT s.first_name, s.last_name, g.group_name FROM students.students s LEFT JOIN students.groups g ON s.group_id=g.id";
-        ResultSet rs = JDBCConnection.selectFromDB(query);
-        rs.last();
-        Assertions.assertEquals(rs.getString("first_name"), "Artem");
+    @DisplayName("Отправка DELETE запроса")
+    public void testDeleteRequest() {
+        String query = "DELETE FROM citizens WHERE ID=412890";
+        JDBCConnection.deleteFromTable(query);
     }
 
     @Test
     @Order(5)
-    @DisplayName("Отправка SELECT JOIN запроса. Проверка фамилии последнего в списке студента")
-    public void testSelectWithJoinRequest_CheckLastName() throws SQLException {
-        String query = "SELECT s.first_name, s.last_name, g.group_name FROM students.students s LEFT JOIN students.groups g ON s.group_id=g.id";
-        ResultSet rs = JDBCConnection.selectFromDB(query);
-        rs.last();
-        Assertions.assertEquals(rs.getString("last_name"), "Pavlov");
+    @DisplayName("Проверка удаления таблицы из БД")
+    public void test_dropTable() {
+        JDBCConnection.dropTable("citizens");
     }
 
     @Test
     @Order(6)
-    @DisplayName("Отправка SELECT JOIN запроса. Проверка группы последнего в списке студента")
-    public void testSelectWithJoinRequest_CheckGroupName() throws SQLException {
-        String query = "SELECT s.first_name, s.last_name, g.group_name FROM students.students s LEFT JOIN students.groups g ON s.group_id=g.id";
-        ResultSet rs = JDBCConnection.selectFromDB(query);
-        rs.last();
-        Assertions.assertEquals(rs.getString("group_name"), "Economic and managment");
+    @DisplayName("Отправка простого SELECT запроса. Проверка адреса")
+    public void testSelectRequest_checkAddress() throws SQLException {
+        String query = "SELECT * FROM address WHERE address_id=1";
+        ResultSet rs = JDBCConnection.selectFromTable(query);
+        String expectedAddress = "47 MySakila Drive";
+        String actualAddress = rs.getString("address");
+        assertEquals(expectedAddress, actualAddress, "Actual address is '" + actualAddress + "'. Expected - '" + expectedAddress + "'.");
     }
 
     @Test
     @Order(7)
-    @DisplayName("Отправка INSERT запроса")
-    public void testInsertRequest() throws SQLException {
-        String query = "INSERT INTO students.disciplines (ID, DISCIPLINE_NAME, HOURS) VALUES ('412890', 'Mathematic analisys', '104')";
-        JDBCConnection.insertIntoDB(query);
-        String selectQuery = "SELECT discipline_name FROM students.disciplines WHERE id=412890";
-        ResultSet rs = JDBCConnection.selectFromDB(selectQuery);
-        rs.first();
-        Assertions.assertEquals(rs.getString("discipline_name"), "Mathematic analisys");
+    @DisplayName("Отправка SELECT JOIN запроса. Проверка принадлежности города стране")
+    public void testSelectWithJoinRequest_CheckCityAndCountry() throws SQLException {
+        String query = "SELECT ct.city, cntr.country FROM city ct LEFT JOIN country cntr ON ct.country_id=cntr.country_id WHERE city='Bratislava'";
+        ResultSet rs = JDBCConnection.selectFromTable(query);
+        String expectedCountry = "Slovakia";
+        String actualCountry = rs.getString("country");
+        assertEquals(expectedCountry, actualCountry, "Actual country is '" + actualCountry + "'. Expected - '" + expectedCountry + "'.");
     }
 
     @Test
     @Order(8)
-    @DisplayName("Отправка DELETE запроса")
-    public void testDeleteRequest() throws SQLException {
-        String query = "DELETE FROM students.disciplines WHERE id=412890";
-        JDBCConnection.deleteFromDB(query);
-    }
-
-    @Test
-    @Order(9)
-    public void testUpdateRequest() throws SQLException {
-        String query = "UPDATE students.students SET first_name = 'Anastasia' WHERE id=11";
-        JDBCConnection.updateInDB(query);
-        String selectQuery = "SELECT first_name FROM students.students WHERE id=11";
-        ResultSet rs = JDBCConnection.selectFromDB(selectQuery);
-        rs.first();
-        Assertions.assertEquals(rs.getString("first_name"), "Anastasia");
+    @DisplayName("Отправка SELECT JOIN запроса. Проверка языка последнего в списке фильма")
+    public void testSelectWithJoinRequest_CheckFilmLanguage() throws SQLException {
+        String query = "SELECT f.title, l.name FROM film f LEFT JOIN language l ON f.language_id=l.language_id";
+        ResultSet rs = JDBCConnection.selectFromTable(query);
+        rs.last();
+        String expectedLanguage = "English";
+        String actualLanguage = rs.getString("name");
+        assertEquals(expectedLanguage, actualLanguage, "Actual language is '" + actualLanguage + "'. Expected - '" + expectedLanguage + "'.");
     }
 
 }
